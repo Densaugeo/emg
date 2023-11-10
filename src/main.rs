2cb1596355f9b5467bedec45256eca36f3bfd395
Densaugeo <>
2023-11-09 20:29:03 -0500

    Trying out Clap for argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,33 @@
-fn main() {
-  // Statements here are executed when the compiled binary is called.
+use clap::Parser;
+
+#[derive(Clone)]
+enum WebAssemblyArg {
+  I32(i32),
+  I64(i64),
+  F32(f32),
+  F64(f64),
+}
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+  /// Name of the person to greet
+  #[arg(short, long)]
+  name: String,
   
-  // Print text to the console.
-  println!("Hello World!");
+  #[arg(long)]
+  arg_test: WebAssemblyArg,
+  
+  /// Number of times to greet
+  #[arg(short, long, default_value_t = 1)]
+  count: u8,
+}
+
+fn main() {
+  let args = Args::parse();
+  
+  for _ in 0..args.count {
+    println!("Hello {}!", args.name)
+  }
 }
