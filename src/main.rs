757ceb7857a0d1444e2328a30a4cc3535e57e6e3
Densaugeo <>
2023-11-12 20:26:12 -0500

    More CLI tool argument handling; added wasmtime

--- src/main.rs
+++ src/main.rs
@@ -1,84 +1,6 @@
+use std::path::PathBuf;
+
 use clap::Parser;
-
-#[derive(Clone, Debug)]
-struct WebAssemblyArgError {}
-
-impl std::fmt::Display for WebAssemblyArgError {
-  fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-    write!(f, "Parameters passed to a WebAssembly module must be numbers. \
-      Maximum of 64 bits (either signed integer or floating point)")
-  }
-}
-
-impl std::error::Error for WebAssemblyArgError {}
-
-#[derive(Clone, Debug)]
-enum WebAssemblyArg {
-  I32(i32),
-  I64(i64),
-  F32(f32),
-  F64(f64),
-}
-
-impl core::str::FromStr for WebAssemblyArg {
-  type Err = WebAssemblyArgError;
-  
-  fn from_str(value: &str) -> Result<Self, Self::Err> {
-    match i32::from_str(value) {
-      Ok(v) => return Ok(Self::I32(v)),
-      _ => {},
-    }
-    
-    match i64::from_str(value) {
-      Ok(v) => return Ok(Self::I64(v)),
-      _ => {},
-    }
-    
-    match f32::from_str(value) {
-      Ok(v) => return Ok(Self::F32(v)),
-      _ => {},
-    }
-    
-    match f64::from_str(value) {
-      Ok(v) => return Ok(Self::F64(v)),
-      _ => {},
-    }
-    
-    Err(Self::Err {})
-  }
-}
-
-#[derive(clap::Subcommand, Debug)]
-enum Subcommands {
-  /// Generate a GLTF model using a Paragen .wasm file
-  Gen {
-    /// Path to .wasm file
-    wasm: String,
-    
-    /// Name of model within .wasm file to generate
-    model: String,
-    
-    /// Parameters to pass to model
-    parameters: Vec<WebAssemblyArg>,
-    
-    /// Not yet implemented - Will be used to specify format of result
-    #[clap(long)]
-    format: Option<u8>,
-  },
-  
-  /// Not yet implemented - Will run a web server for viewing a Paragen .wasm
-  /// file
-  Serve {
-    /// Path to .wasm file
-    wasm: String,
-  },
-  
-  /// Not yet implemented - Will report metadata from a Paragen .wasm file
-  Inspect {
-    /// Path to .wasm file
-    wasm: String,
-  }
-}
 
 /// Paragen by Den Antares
 ///
@@ -90,22 +12,205 @@ struct Cli {
   subcommand: Subcommands,
 }
 
+#[derive(clap::Subcommand, Debug)]
+enum Subcommands {
+  /// Generate a GLTF model using a Paragen .wasm file
+  Gen(ArgsForGen),
+  
+  /// Not yet implemented - Will run a web server for viewing a Paragen .wasm
+  /// file
+  Serve(ArgsForServe),
+  
+  /// Not yet implemented - Will report metadata from a Paragen .wasm file
+  Inspect(ArgsForInspect),
+}
+
+#[derive(clap::Args, Debug)]
+struct ArgsForGen {
+  /// Path to .wasm file
+  wasm: PathBuf,
+  
+  /// Name of model within .wasm file to generate
+  model: String,
+  
+  /// Parameters to pass to model
+  model_parameters: Vec<String>,
+  
+  /// Not yet implemented - Will be used to specify format of result
+  #[clap(long)]
+  format: Option<u8>,
+}
+
+#[derive(clap::Args, Debug)]
+struct ArgsForServe {
+  /// Path to .wasm file
+  wasm: PathBuf,
+}
+
+#[derive(clap::Args, Debug)]
+struct ArgsForInspect {
+  /// Path to .wasm file
+  wasm: PathBuf,
+}
+
+fn gen(args: ArgsForGen) {
+  let engine = wasmtime::Engine::default();
+  
+  let module = match wasmtime::Module::from_file(&engine, args.wasm) {
+    Ok(m) => m,
+    Err(e) => {
+      println!("Error: {:?}", e);
+      std::process::exit(2);
+    },
+  };
+  
+  // The "store" seems to be wasmtime's container for instance data
+  let mut store = wasmtime::Store::new(&engine, ());
+  
+  let instance = match wasmtime::Instance::new(&mut store, &module, &[]) {
+    Ok(i) => i,
+    Err(e) => {
+      println!("Error: {:?}", e);
+      std::process::exit(3);
+    },
+  };
+  
+  let generate_model = match instance.get_func(&mut store,
+    (String::from("paragen_") + &args.model).as_str(),
+  ) {
+    Some(f) => f,
+    None => {
+      println!("Error: .wasm file does not contain the model `{}`", args.model);
+      std::process::exit(4);
+    }
+  };
+  
+  let parameter_count = generate_model.ty(&store).params().len();
+  if args.model_parameters.len() != parameter_count {
+    println!("Error: model expects {} parameters, but {} were given",
+      parameter_count, args.model_parameters.len());
+    std::process::exit(5);
+  }
+  
+  let mut model_args: Vec<wasmtime::Val> = Vec::new();
+  let mut i = 0;
+  for parameter in generate_model.ty(&store).params() {
+    match parameter {
+      wasmtime::ValType::I32 => {
+        match args.model_parameters[i].parse::<i32>() {
+          Ok(v) => model_args.push(wasmtime::Val::from(v)),
+          Err(_) => {
+            println!("Error: model parameter {} (`{}`) should be a 32-bit \
+              integer", i + 1, args.model_parameters[i]);
+            std::process::exit(7);
+          },
+        }
+      },
+      
+      wasmtime::ValType::I64 => {
+        match args.model_parameters[i].parse::<i64>() {
+          Ok(v) => model_args.push(wasmtime::Val::from(v)),
+          Err(_) => {
+            println!("Error: model parameter {} (`{}`) should be a 64-bit \
+              integer", i + 1, args.model_parameters[i]);
+            std::process::exit(7);
+          },
+        }
+      },
+      
+      wasmtime::ValType::F32 => {
+        match args.model_parameters[i].parse::<f32>() {
+          Ok(v) => model_args.push(wasmtime::Val::from(v)),
+          Err(_) => {
+            println!("Error: model parameter {} (`{}`) should be a 32-bit \
+              floating-point value", i + 1, args.model_parameters[i]);
+            std::process::exit(7);
+          },
+        }
+      },
+      
+      wasmtime::ValType::F64 => {
+        match args.model_parameters[i].parse::<f64>() {
+          Ok(v) => model_args.push(wasmtime::Val::from(v)),
+          Err(_) => {
+            println!("Error: model parameter {} (`{}`) should be a 64-bit \
+              floating-point value", i + 1, args.model_parameters[i]);
+            std::process::exit(7);
+          },
+        }
+      },
+      
+      _ => {
+        println!("Error: paragen models only support parameters of type i32, \
+          i64, f32, or f64");
+        std::process::exit(6);
+      }
+    };
+    
+    i += 1;
+  }
+  
+  println!("Extracted arguments for passing through to model: {:?}", model_args);
+  
+  let result_count = generate_model.ty(&store).results().len();
+  if result_count != 1 {
+    println!("Error: paragen models must return a 32-bit integer error code");
+    std::process::exit(8);
+  }
+  
+  // .unwrap() acceptable here because the length is asserted == 1
+  match generate_model.ty(&store).results().next().unwrap() {
+    wasmtime::ValType::I32 => {},
+    _ => {
+      println!("Error: paragen models must return a 32-bit integer error code");
+      std::process::exit(8);
+    }
+  }
+  
+  println!("Trying to call some paragen functions...");
+  let mut result = [wasmtime::Val::from(0)];
+  match generate_model.call(&mut store, &model_args, &mut result) {
+    Ok(_) => {},
+    Err(e) => {
+      println!("Error: {:?}", e);
+      std::process::exit(9);
+    },
+  }
+  // .unwrap() acceptable here because the type was previously asserted
+  match result[0].i32().unwrap() {
+    0 => {},
+    e => {
+      println!("Error: model generation returned error code: {}", e);
+      std::process::exit(10);
+    },
+  }
+  
+  let mut pointer = [wasmtime::Val::from(0)];
+  instance.get_func(&mut store, "pointer").unwrap().call(&mut store, &[], &mut pointer).unwrap();
+  println!("Got pointer: {:?}", pointer);
+  
+  let mut size = [wasmtime::Val::from(0)];
+  instance.get_func(&mut store, "size").unwrap().call(&mut store, &[], &mut size).unwrap();
+  println!("Got size: {:?}", size);
+  
+}
+
+fn serve(_args: ArgsForServe) {
+  println!("Not implemented yet");
+  std::process::exit(1);
+}
+
+fn inspect(_args: ArgsForInspect) {
+  println!("Not implemented yet");
+  std::process::exit(1);
+}
+
 fn main() {
   let args = Cli::parse();
   
   match args.subcommand {
-    Subcommands::Gen {
-      format,
-      wasm,
-      model,
-      parameters,
-    } => {
-      println!("Gen: format={:?}, wasm={}, model={}", format, wasm, model);
-      for i in parameters {
-        println!("{:?}", i);
-      }
-    },
-    Subcommands::Serve { .. } => println!("Not yet implemented"),
-    Subcommands::Inspect { .. } => println!("Not yet implemented"),
+    Subcommands::Gen(args) => gen(args),
+    Subcommands::Serve(args) => serve(args),
+    Subcommands::Inspect(args) => inspect(args),
   }
 }
